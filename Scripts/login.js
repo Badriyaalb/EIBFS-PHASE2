cf743c88b283d8fc4971b49faa3a901a2936309f
Badriyaalb <124551518+Badriyaalb@users.noreply.github.com>
2023-02-16 14:46:22 +0530

    Add files via upload

--- Scripts/login.js
+++ Scripts/login.js
@@ -1,30 +1,23 @@
-function validateForm() {
-  validateUsername();
-  validatePassword();
-}
 
-function validateUsername() {
-  let userName = document.forms["loginForm"]["userName"].value;
-  if (userName.length == 0) {
-    document.getElementById("userNameError").innerText =
-      "Please enter a username";
-    return false;
-  } else if (userName.length <= 8 || userName.length >= 20) {
-    document.getElementById("userNameError").innerText =
-      "Please enter a username between 8 and 20 characters";
-    return false;
-  }
-}
 
-function validatePassword() {
-  let userPassword = document.forms["loginForm"]["userPassword"].value;
-  if (userPassword.length == 0) {
-    document.getElementById("userPasswordError").innerText =
-      "Please enter a password";
-    return false;
-  } else if (userPassword.length <= 8 || userPassword.length >= 20) {
-    document.getElementById("userPasswordError").innerText =
-      "Please enter a password between 8 and 20 characters";
-    return false;
-  }
+function validatePassword(){
+    let userPassword = document.forms["loginForm"]["userPassword"].value;
+    let lowerCaseCheck = /[a-z]/g;
+    let upperCaseCheck = /[A-Z]/g;
+    let numberCheck = /[0-9]/g;
+    if(userPassword.match(lowerCaseCheck)){
+        document.getElementById("passwordErrorLC").innerText="lowercase provided";
+    } else {
+        document.getElementById("passwordErrorLC").innerText="lowercase required";
+    }
+    if(userPassword.match(upperCaseCheck)){
+        document.getElementById("passwordErrorUC").innerText="uppercase provided";
+    } else {
+        document.getElementById("passwordErrorUC").innerText="uppercase required";
+    }
+    if(userPassword.match(numberCheck)){
+        document.getElementById("passwordErrorN").innerText="number provided";
+    } else {
+        document.getElementById("passwordErrorN").innerText="number required";
+    }
 }
