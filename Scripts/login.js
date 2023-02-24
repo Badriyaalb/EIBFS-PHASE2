312e97276af7b83371b66cca69ce5b351bc6d07b
Badriyaalb <124551518+Badriyaalb@users.noreply.github.com>
2023-02-24 10:05:57 +0400

    Add files via upload

--- Scripts/login.js
+++ Scripts/login.js
@@ -5,19 +5,17 @@ function validatePassword(){
     let lowerCaseCheck = /[a-z]/g;
     let upperCaseCheck = /[A-Z]/g;
     let numberCheck = /[0-9]/g;
-    if(userPassword.match(lowerCaseCheck)){
-        document.getElementById("passwordErrorLC").innerText="lowercase provided";
-    } else {
-        document.getElementById("passwordErrorLC").innerText="lowercase required";
+    
+    document.getElementById("passwordError").innerText="";
+    console.log("Test Change");
+
+    if(!userPassword.match(lowerCaseCheck))
+    {document.getElementById("passwordError").innerText="lowercase required";
     }
-    if(userPassword.match(upperCaseCheck)){
-        document.getElementById("passwordErrorUC").innerText="uppercase provided";
-    } else {
-        document.getElementById("passwordErrorUC").innerText="uppercase required";
+    if(!userPassword.match(upperCaseCheck))
+    {document.getElementById("passwordError").innerText+=" uppercase required";
     }
-    if(userPassword.match(numberCheck)){
-        document.getElementById("passwordErrorN").innerText="number provided";
-    } else {
-        document.getElementById("passwordErrorN").innerText="number required";
+    if(!userPassword.match(numberCheck))
+    {document.getElementById("passwordError").innerText+=" number required";
     }
 }
